fda2c42d3100d3cfda60decdf449a6f1d5309255
Varia S <especificita@gmail.com>
2022-07-10 12:33:13 +0300

    Added Task 31

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿void Task31()
+{
+Console.WriteLine("Задача 31: Задайте массив из 12 элементов, заполненный случайными числами. Найдите сумму отрицательных и положительных элементов массива.");
+
+int size = 12;
+int [] array = new int[size];
+FillArray(array, -5, 5);
+PrintArray(array);
+Console.WriteLine("Сумма отрицательных элементов массива: " + SumNegative(array));
+Console.WriteLine("Сумма положительных элементов массива: " + SumPositive(array));
+
+}
+int SumNegative(int [] array)
+{
+    int sumNegative = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if (array[i] < 0) sumNegative += array[i];    
+    }
+    return sumNegative;
+    
+}
+
+int SumPositive(int [] array)
+{    
+    int sumPositive = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if (array[i] > 0) sumPositive += array[i];
+    }
+    return sumPositive;
+}
+
+void FillArray(int[] array, int startNumber = 0, int endNumber = 0)
+{
+    endNumber++;
+    Random random = new Random();
+    for (int i = 0; i <array.Length; i++)
+    {
+        array[i] = random.Next(startNumber,endNumber);
+        
+    }
+        
+}
+void PrintArray (int [] array)
+{
+    for (int i = 0; i <array.Length; i++)
+    {
+        
+        Console.Write(array[i] + " ");
+        
+    }
+    Console.WriteLine();
+}
+Task31();
